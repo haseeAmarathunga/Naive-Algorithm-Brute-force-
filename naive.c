7441f58da575420221e288983bd8486783e7e21e
Hasee Amarathunga <haseeamarathunga@gmail.com>
2018-05-24 20:38:02 -0700

    Added naive.c file.

---
+++ naive.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+
+void naive(char*txt,char*pat)
+{
+	int matchCount;
+	int n,m,i,j;
+	n=strlen(txt);
+	m=strlen(pat);
+	
+	for (i=0;i<=n-m;i++){
+		
+		matchCount=0;
+		
+		for (j=0;j<m;j++){
+			if (txt[i+j]==pat[j]){
+				matchCount++;
+			}
+			else{
+				break;
+			}
+		}
+		if (matchCount==m){
+			printf("Pattern Found at index %d.\n",i);
+		}
+	}
+	
+}
+
+void main()
+{
+	char txt[]="Leavemealoneve";
+	char pat[]="ve";
+	naive(txt,pat);
+	return 0;
+}
+
